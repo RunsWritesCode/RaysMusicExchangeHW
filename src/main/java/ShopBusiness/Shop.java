9559dba42e2a5091037a9df8bfa38ba84a99dfbf
RunsWritesCode <sophieamullins@gmail.com>
2017-12-15 15:09:37 +0000

    Shop class added with getters and setters, stock count method added

--- src/main/java/ShopBusiness/Shop.java
+++ src/main/java/ShopBusiness/Shop.java
@@ -1,4 +1,31 @@
 package ShopBusiness;
 
+import java.util.ArrayList;
+
 public class Shop {
+
+    private ArrayList<ISell> stock;
+
+    public Shop() {
+        stock = new ArrayList<ISell>();
+
+    }
+
+    public ArrayList<ISell> getStock() {
+        return stock;
+    }
+
+    public void setStock(ArrayList<ISell> stock) {
+        this.stock = stock;
+    }
+
+
+    public int stockCount() {
+        return this.stock.size();
+    }
+
+
+
+
+
 }
---
+++ src/test/java/ShopTest.java
@@ -0,0 +1,41 @@
+import Instruments.Flute;
+import Instruments.Guitar;
+import Instruments.Piano;
+import Miscellaneous.Plectrum;
+import Miscellaneous.SheetMusic;
+import ShopBusiness.Shop;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShopTest {
+
+    Shop shop;
+    Flute flute;
+    Piano piano;
+    SheetMusic sheetMusic;
+    Plectrum plectrum;
+    Guitar guitar;
+
+
+    @Before
+        public void before() {
+            shop = new Shop();
+            flute = new Flute("woodwind", "Pearl", "Quantz Forza", 1023.44, "gold");
+            plectrum = new Plectrum("wood", "Manics", 4, 9.99);
+            guitar = new Guitar("String","Gibson", "Les Paul", 1800.04, 6);
+    }
+
+    @Test
+    public void stockCountStartsAtZero() {
+        assertEquals(0, shop.stockCount());
+    }
+
+    @Test
+    public void addItemToStock() {
+        assertEquals(1, shop.stockCount());
+    }
+}
